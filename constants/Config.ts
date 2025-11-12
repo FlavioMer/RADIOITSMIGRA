bbdfb11faf08a7276e94abc1cabbfd6d220d2c67
Flavio Merecías <mereflv17@gmail.com>
2025-11-12 14:26:53 -0600

    Primer Avance

---
+++ constants/Config.ts
@@ -0,0 +1,21 @@
+//Exportar la configuración de la Radio
+
+import { Background } from "@react-navigation/elements";
+
+export const RADIO_CONFIG = {
+    station: {
+        name: "Radio ITSMIGRA",
+        frecuency: "96.5 FM",
+        location: "San Miguel el Grande",
+        streamURL: "https://www.smiguelgde.tecnm.mx/stream",
+        website: "https://www.smiguelgde.tecnm.mx/radio.html",
+        phone: "+52 9533079303",
+        logo: require("../assets/images/Logo_Radio.png")
+    },
+    colors:{
+        primary: "#1E40AF", //Azul Institucional
+        secondary: "#DC2626", //Rojo    
+        Background: "#0F172A", //Fondo Oscuro
+        text: "#F8FAFC", //Texto Claro
+    }
+}   
